87322e7fb26925c0fbc754a903db22ebe74bc11b
Alexis Reyna <alexis.reynasz@uanl.edu.mxw>
2026-06-10 15:46:18 -0600

    Img config file

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+      },
+      {
+        protocol: "https",
+        hostname: "s2.coinmarketcap.com",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
